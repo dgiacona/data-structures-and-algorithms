6cd08552ac9c5c19ee8739a6bcca87ef943ffb22
Dominic Giacona <dominic@exobyte.io>
2022-06-21 23:02:07 -0400

    answered number 6 for coding challenge 1

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -113,7 +113,13 @@ This function should use forEach to populate your grocery list based on the stor
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
-  // Solution code here...
+  let newList=[];
+  availableItems.forEach((item) => {
+    if(item.available===true){
+      newList.push(item.name);
+    }
+  });
+  return newList;
 };
 
 /* ------------------------------------------------------------------------------------------------
