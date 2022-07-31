6117e1929886c41b6c4b16945b6a3dd2064f56aa
Dominic Giacona <dominic@exobyte.io>
2022-07-31 04:43:10 -0400

    code challenge 6 complete

--- python/data_structures/linked_list.py
+++ python/data_structures/linked_list.py
@@ -32,11 +32,62 @@ class LinkedList:
         new_node.next = self.head
         self.head = new_node
 
+    def append(self, value):
+        new_node = Node(value)
+        current = self.head
+        while current:
+            if not current.next:
+                current.next = new_node
+
+                break
+
+            else:
+                current = current.next
+
+    def insert_before(self, search_val, value):
+        new_node = Node(value)
+
+        if not self.head:
+            raise TargetError
+
+        if self.head.value == search_val:
+            self.insert(value)
+            return
+
+        current = self.head
+
+        while current and current.next:
+            if current.next.value == search_val:
+                new_node.next = current.next
+                current.next = new_node
+                return
+            else:
+                current = current.next
+        raise TargetError
+
+    def insert_after(self, search_val, value):
+
+        current = self.head
+        if not self.head:
+            raise TargetError
+
+        while current:
+            if current.value == search_val:
+                self.insert_before(current.next.value, value)
+                return
+            else:
+                current = current.next
+        raise TargetError
+
+
+
+
+
 class Node:
     def __init__(self, value, next=None):
         self.value = value
         self.next = next
 
 
-class TargetError:
+class TargetError(Exception):
     pass
