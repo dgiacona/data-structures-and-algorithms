58d90fd47490fd8a96ef06a7cf5693cd7f37dfb7
Dominic Giacona <dominic@exobyte.io>
2022-08-02 02:24:48 -0400

    completed challenge 10

--- python/data_structures/queue.py
+++ python/data_structures/queue.py
@@ -1,12 +1,50 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+
 class Queue:
     """
-    Put docstring here
+    Create a Queue:
+    front, rear, next
     """
 
     def __init__(self):
         # initialization here
-        pass
+        self.front = None
+        self.rear = None
 
-    def some_method(self):
+    def enqueue(self, value):
         # method body here
-        pass
+        new_rear = Node(value)
+        if not self.rear:
+            self.rear = new_rear
+            self.front = new_rear
+        else:
+            old_rear = self.rear
+            old_rear.next = new_rear
+            self.rear = new_rear
+
+    def dequeue(self):
+        if not self.front:
+            raise InvalidOperationError
+        else:
+            # remove front
+            # reset front
+            # return value
+            old_front = self.front
+            self.front = self.front.next
+            return old_front.value
+
+    def peek(self):
+        if not self.front:
+            raise InvalidOperationError()
+        return self.front.value
+
+    def is_empty(self):
+        if not self.front:
+            return True
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,12 +1,41 @@
+from data_structures.invalid_operation_error import InvalidOperationError
+
+
 class Stack:
     """
-    Put docstring here
+    Create a stack:
+    Top, next
     """
 
     def __init__(self):
         # initialization here
-        pass
+        self.top = None
 
-    def some_method(self):
+    def push(self, value):
         # method body here
-        pass
+        node = Node(value, self.top)
+        self.top = node
+
+    def pop(self):
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        else:
+            old_top = self.top
+            self.top = self.top.next
+            return old_top.value
+
+    def is_empty(self):
+        if not self.top:
+            return True
+
+    def peek(self):
+        if self.is_empty():
+            raise InvalidOperationError("Method not allowed on empty collection")
+        else:
+            return self.top.value
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
