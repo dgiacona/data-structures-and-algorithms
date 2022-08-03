dd221c2cad5066cae1dfb435eb72bdca0d26de26
Dominic Giacona <dominic@exobyte.io>
2022-08-03 02:28:26 -0400

    completed lab 11

--- python/code_challenges/stack_queue_pseudo.py
+++ python/code_challenges/stack_queue_pseudo.py
@@ -2,4 +2,44 @@ from data_structures.stack import Stack
 
 
 class PseudoQueue:
-    pass
+    def __init__(self):
+        # initialization here
+        self.front = None
+        self.rear = None
+
+    def enqueue(self, value):
+        # method body here
+        new_rear = Node(value)
+        if not self.rear:
+            self.rear = new_rear
+            self.front = new_rear
+        else:
+            old_rear = self.rear
+            old_rear.next = new_rear
+            self.rear = new_rear
+
+    def dequeue(self):
+        if not self.front:
+            raise InvalidOperationError
+        else:
+            # remove front
+            # reset front
+            # return value
+            old_front = self.front
+            self.front = self.front.next
+            return old_front.value
+
+    def peek(self):
+        if not self.front:
+            raise InvalidOperationError()
+        return self.front.value
+
+    def is_empty(self):
+        if not self.front:
+            return True
+
+
+class Node:
+    def __init__(self, value, next_=None):
+        self.value = value
+        self.next = next_
