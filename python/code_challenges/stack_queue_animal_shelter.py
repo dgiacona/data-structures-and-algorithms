72e5130595f192f08d25ed1868eec7ba57ef7d2a
Dominic Giacona <dominic@exobyte.io>
2022-08-05 04:08:18 -0400

    completed code challenge 12

--- python/code_challenges/stack_queue_animal_shelter.py
+++ python/code_challenges/stack_queue_animal_shelter.py
@@ -2,12 +2,37 @@ from data_structures.queue import Queue
 
 
 class AnimalShelter:
-    pass
+    def __init__(self):
+        self.dog_queue = Queue()
+        self.cat_queue = Queue()
 
+    def enqueue(self, animal):
+        if animal.animal_type == "dog":
+            self.dog_queue.enqueue(animal)
+
+        elif animal.animal_type == "cat":
+            self.cat_queue.enqueue(animal)
+
+        else:
+            raise Queue.InvalidOperationError
+
+
+    def dequeue(self, value):
+        if value == "dog":
+            return self.dog_queue.dequeue()
+
+        if value == "cat":
+            return self.cat_queue.dequeue()
+
+        else:
+            return None
 
 class Dog:
-    pass
+    def __init__(self):
+        self.animal_type = "dog"
 
 
 class Cat:
-    pass
+    def __init__(self):
+        self.animal_type = "cat"
+
