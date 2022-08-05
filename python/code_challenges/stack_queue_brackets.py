9e5ad9a342743c79ee1b76344dad8a98f9238b95
Dominic Giacona <dominic@exobyte.io>
2022-08-05 05:53:10 -0400

    code challenge 13

--- python/code_challenges/stack_queue_brackets.py
+++ python/code_challenges/stack_queue_brackets.py
@@ -1,5 +1,33 @@
 from data_structures.queue import Queue
+from data_structures.stack import Stack
 
+open_bracks = Stack()
+openers = ["(", "[", "{"]
+closers = [")", "]", "}"]
+matches = {
+    ')': '(',
+    ']': '[',
+    '}': '{'
+}
 
-def multi_bracket_validation():
-    pass
+def multi_bracket_validation(bracks):
+    if len(bracks) // 2 != 0:
+        return False
+    elif bracks[0] in closers:
+        return False
+    else:
+        for x in len(bracks):
+            if bracks[x] in openers:
+                open_bracks.push(x)
+            else:
+                print("hello world")
+                match_combo = (bracks[0] + open_bracks.peek())
+                print(match_combo)
+                if (bracks[0] + open_bracks.peek()) in matches:
+                    open_bracks.pop()
+                else:
+                    return False
+
+#             print(open_bracks)
+#
+multi_bracket_validation("[[{}]]")
