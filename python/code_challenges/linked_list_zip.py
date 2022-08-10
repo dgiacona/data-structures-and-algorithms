7a6ece41b55d375273f3dd92ec42c746131b1236
Dominic Giacona <dominic@exobyte.io>
2022-08-10 03:07:10 -0400

    3 test pass working through it want to submit for credit

--- python/code_challenges/linked_list_zip.py
+++ python/code_challenges/linked_list_zip.py
@@ -1,2 +1,21 @@
+from data_structures.linked_list import LinkedList
+
 def zip_lists(a, b):
-    pass
+
+    list_one = a.head
+    list_two = b.head
+    while list_two and list_one:
+        a.append(list_one.value, list_two.value)
+        list_one = list_one.next
+        if list_one.next:
+            list_one = list_one.next
+        list_two = list_two.next
+
+    if list_one is None or list_two is None:
+        if list_two:
+            return b
+        if list_one:
+            return a
+
+    return a
+
