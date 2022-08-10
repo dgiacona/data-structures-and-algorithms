c623deb62b1bfed60fb1544f99300d04d273c374
Dominic Giacona <dominic@exobyte.io>
2022-08-10 00:44:04 -0400

    working through challenge 16

--- python/code_challenges/tree_breadth_first.py
+++ python/code_challenges/tree_breadth_first.py
@@ -1,7 +1,28 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.queue import Queue
 
 
-def breadth_first():
+def breadth_first(tree):
+    tree_breadth = Queue()
     values = []
 
+    if tree.root:
+        tree_breadth.enqueue(tree.root)
+
+    if tree.root:
+        return values
+
+    while not tree_breadth.is_empty():
+        node_front = tree_breadth.front.value
+
+
+        if node_front.left:
+            tree_breadth.enqueue(node_front.left)
+
+        if node_front.right:
+            tree_breadth.enqueue(node_front.right)
+
+        values.append(tree_breadth.dequeue().value)
+
+    return values
 
