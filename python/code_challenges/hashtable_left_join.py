12caa6b309de089c7513ad77b8af1956dd02d09d
Dominic Giacona <dominic@exobyte.io>
2022-09-19 07:31:04 -0400

    code challenge 33

--- python/code_challenges/hashtable_left_join.py
+++ python/code_challenges/hashtable_left_join.py
@@ -1,5 +1,13 @@
 from data_structures.hashtable import Hashtable
 
 
-def left_join():
-    pass
+def left_join(table_a, table_b):
+    return_list = []
+
+    for key, value in table_a.items():
+        if table_b.get(key):
+            return_list.append([key, value, table_b.get(key)])
+        else:
+            return_list.append([key, value, 'NONE'])
+
+    return return_list
