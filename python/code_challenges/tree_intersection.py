14e35a045d784d54b12b684134b93614182dc9aa
Dominic Giacona <dominic@exobyte.io>
2022-09-19 07:23:24 -0400

    code challenge class 32

--- python/code_challenges/tree_intersection.py
+++ python/code_challenges/tree_intersection.py
@@ -1,5 +1,17 @@
 from data_structures.binary_tree import BinaryTree
+from data_structures.hashtable import Hashtable
 
 
-def tree_intersection():
-    pass
+def tree_intersection(tree_a, tree_b):
+    common_numbers = []
+
+    ht = Hashtable()
+    for num in tree_a.pre_order():
+        ht.set(num, num)
+
+    for num in tree_b.pre_order():
+        if ht.contains(num):
+            if num not in common_numbers:
+                common_numbers.append(num)
+
+    return common_numbers
